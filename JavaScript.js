6b060b7f843dc320d768eed407f7da505edd345b
arzuagamanuel <71405710+ManuelArzuaga@users.noreply.github.com>
2024-01-31 14:13:52 -0300

    commit-two

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,6 @@
 const img = document.querySelector("img");
 
+
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=ifEY0JLh4RWc8T20UnwyzloArPRNeYdU&s=cats', {mode: 'cors'})
 .then(function(response){ //devuelve una promesa por eso hay que hacer otro then
   return response.json()
